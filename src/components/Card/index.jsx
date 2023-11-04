6d38498b2168f7b6ed62620c0d5100be29aa2932
Lukasz Nogaj <lukasznogaj@Lukaszs-Mac-mini.local>
2023-11-04 13:50:33 +0000

    start-project

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import styles from './index.module.scss';
+const Card = () => {
+  return (
+    <div className={styles.hero}>
+      <h1>Card</h1>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/DataForProgress/index.jsx
@@ -0,0 +1,28 @@
+// const dataForProgress = [
+//   {
+//     id: 1,
+//     // image: pictureOne,
+//     paragraph:
+//       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+//     head: 'Lee Stewart',
+//     heading: 'Senior Developer',
+//   },
+//   {
+//     id: 2,
+//     // image: pictureTwo,
+//     paragraph:
+//       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+//     head: 'Alina Coachella',
+//     heading: 'Senior Designer ',
+//   },
+//   {
+//     id: 3,
+//     // image: pictureThree,
+//     paragraph:
+//       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+//     head: 'Zola Lee',
+//     heading: 'CEO ',
+//   },
+// ];
+
+// export default dataForUsers;
